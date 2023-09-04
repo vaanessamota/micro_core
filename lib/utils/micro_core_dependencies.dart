a9d1f29a752e28e452fa7427c27df649056b04f4
vaanessamota <vanessaoliveira2706@gmail.com>
2023-09-03 23:15:55 -0300

    feat(presenter): add login presenter

--- lib/utils/micro_core_dependencies.dart
+++ lib/utils/micro_core_dependencies.dart
@@ -2,6 +2,8 @@ import 'package:micro_core/core/http/default_http_client.dart';
 import 'package:micro_core/core/http/http_client.dart';
 import 'package:micro_core/utils/app_locator.dart';
 
+final locator = AppLocator();
+
 class MicroCoreDependencies {
   static setUp(AppLocator locator) {
     locator.registerFactory<HttpClient>(() => DefaultHttpClient());
